6077d39e9b0d3ce20dba522815c9e46319105e03
Morten Sjøgren <m_abs@mabs.dk>
2016-05-12 22:11:28 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,10 +2,18 @@
 
 set -e
 
+cd $(dirname $0)
+HERE=$PWD
+
 git pull
+git submodule foreach git clean -d -x -f
 git submodule foreach git checkout master
 git submodule foreach git pull origin master
 git submodule update --init --recursive
 git add -p sources_non_forked
 
-cd sources_non_forked/YouCompleteMe/ && ./install.py --clang-completer --system-libclang && cd -
+cd $HERE/sources_non_forked/YouCompleteMe
+./install.py --clang-completer --system-libclang
+
+cd $HERE/sources_non_forked/vimproc.vim
+make -j4
